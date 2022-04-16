cc423e30b004524545a2bdc63bb76b7bbd966304
Sofia Mudry <sofiamudry@MBP-de-Sofi.fibertel.com.ar>
2022-04-16 16:22:16 -0300

    Corrijo semantica

--- app.js
+++ app.js
@@ -2,17 +2,6 @@ console.log('Incremento');
 
 alert('Incremento');
 
-/*
-const base = prompt ('Ingrese el numero base');
-const cantidad = prompt ('Ingrese una cantidad a ir incrementando');
-const incremento01 = Number(base) + Number(cantidad);
-const incremento02 = Number(base) + Number(cantidad) + Number(cantidad);
-const incremento03 = Number(base) + Number(cantidad) + Number(cantidad) + Number(cantidad);
-const incremento04 = Number(base) + Number(cantidad) + Number(cantidad) + Number(cantidad) + Number(cantidad);
-const incremento05 = Number(base) + Number(cantidad) + Number(cantidad) + Number(cantidad) + Number(cantidad) + Number(cantidad);
-*/
-
-
 const base = prompt ('Ingrese el numero base');
 const cantidad = prompt ('Ingrese una cantidad a ir incrementando');
 const incremento01 = Number(base) + Number(cantidad);
@@ -20,7 +9,6 @@ const incremento02 = Number(base) + Number(cantidad)*2;
 const incremento03 = Number(base) + Number(cantidad)*3;
 const incremento04 = Number(base) + Number(cantidad)*4;
 const incremento05 = Number(base) + Number(cantidad)*5;
-
 
 alert(`Incremento 01) ${incremento01}`);
 alert(`Incremento 02) ${incremento02}`);
